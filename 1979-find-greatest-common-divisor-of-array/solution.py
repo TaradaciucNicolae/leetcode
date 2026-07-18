b94aebd0f6d2ec24db7090521cf95d40961fc049
Taradaciuc Nicolae <126168262+TaradaciucNicolae@users.noreply.github.com>
2026-07-18 16:26:03 +0300

    Sync solution.py for 1979. Find Greatest Common Divisor of Array

---
+++ 1979-find-greatest-common-divisor-of-array/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        
+        smallest = min(nums)
+
+        largest = max(nums)
+
+        reversed_counter = smallest
+
+        while reversed_counter != 0:
+
+            if smallest % reversed_counter == 0 and largest % reversed_counter == 0:
+                return reversed_counter
+
+
+            reversed_counter -= 1
