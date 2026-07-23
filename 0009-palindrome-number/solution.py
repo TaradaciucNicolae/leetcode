1a7af5bc755afe4fefe31a827513684cb0c199dc
Taradaciuc Nicolae <126168262+TaradaciucNicolae@users.noreply.github.com>
2026-07-23 11:55:55 +0300

    Runtime: 0 ms (100.00%), Memory: 19.1 MB (86.33%) - Python - SafeLeetSync Auto Commit

--- 0009-palindrome-number/solution.py
+++ 0009-palindrome-number/solution.py
@@ -1,14 +1,32 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
+    
+        
+        string_x = str(x)
+        reversed_string_x = string_x[::-1]
+
+        #print(x, string_x)
+        if reversed_string_x == string_x:
+            return True
+        else:
             return False
+     
+     
+     
+     
+     
+     
+        # if x < 0:
+        #     return False
 
-        original = x
-        reversed_num = 0
+        # original = x
+        # reversed_num = 0
 
-        while x > 0:
-            digit = x % 10
-            reversed_num = reversed_num * 10 + digit
-            x //= 10
+        # while x > 0:
+        #     digit = x % 10 # grabbing the last digit
 
-        return original == reversed_num
+        #     reversed_num = reversed_num * 10 + digit # adding the last digit to the reversed number
+
+        #     x //= 10 # Removing the last digit the we previously used
+
+        # return original == reversed_num
